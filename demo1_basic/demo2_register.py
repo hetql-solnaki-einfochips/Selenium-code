9471994b2a270eda7ce28d850ac2c157cdad92c0
Hetal Solanki <Hetal.Solanki@einfochips.com>
2023-02-24 11:12:43 +0530

    Selenium code date 24

--- demo1_basic/demo2_register.py
+++ demo1_basic/demo2_register.py
@@ -2,6 +2,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 driver=webdriver.Chrome()
 driver.get("https://www.facebook.com/")
@@ -12,7 +13,14 @@ driver.find_element(By.NAME,"lastname").send_keys("Solanki")
 driver.find_element(By.NAME,"reg_email__").send_keys("john@gmail.com")
 driver.find_element(By.NAME,"reg_passwd__").send_keys("Snow123@#")
 driver.implicitly_wait(5)
-#driver.find_element(By.ID,"day").
+select_day=Select(driver.find_element(By.ID,"day"))
+select_day.select_by_visible_text('20')
+
+select_month=Select(driver.find_element(By.ID,"month"))
+select_month.select_by_visible_text('Dec')
+
+select_year=Select(driver.find_element(By.ID,"year"))
+select_year.select_by_visible_text('2008')
 
 driver.find_element(By.XPATH,"//label[text()='Custom']").click() #Xpath: //tagname[@attribute=''] or /tagname[text()='']
 driver.find_element(By.NAME, "websubmit").click()
