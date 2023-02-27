ee86ef838cfacfe94b0fe0219767d9decfec9139
Hetal Solanki <Hetal.Solanki@einfochips.com>
2023-02-27 12:36:06 +0530

    Added for frame

--- demo2_tabs/demo1_dbfree.py
+++ demo2_tabs/demo1_dbfree.py
@@ -9,11 +9,25 @@ driver.get("https://www.db4free.net/")
 driver.find_element(By.XPATH,"//a[@href='/phpMyAdmin']").click()
 print(driver.title)
 
-driver.switch_to.window(driver.window_handles[1])
+#driver.switch_to.window(driver.window_handles[1]) # second/new tab
+print(driver.window_handles)
 
-driver.find_element(By.ID,'input_username').send_keys("Heta")
-driver.find_element(By.ID,'input_password').send_keys("hetal2325")
+for window in driver.window_handles:
+    driver.implicitly_wait(5)
+    print(window)
+    driver.switch_to.window(window)
+    print(driver.title)
+    if driver.title == 'phpMyAdmin':
+        break
+
+    driver.implicitly_wait(5)
+    print('------------')
+
+
+driver.find_element(By.ID, 'input_username').send_keys("Heta")
+driver.find_element(By.ID, 'input_password').send_keys("hetal2325")
 driver.find_element(By.ID, 'input_go').click()
+
 
 time.sleep(5)
 driver.quit()
